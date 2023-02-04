79018b0fe56cbbda0ab1f0945d258323ee66a2b9
emilhrdnsy <emilhardiansyah54@gmail.com>
2023-02-04 22:14:11 +0800

    fix login gagal

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
- <title>Login Gagal ! - Chirexs 1.0</title>
+ <title>Login Gagal!</title>
 <?php
 session_start();
 include "config/koneksi.php";
@@ -21,36 +21,18 @@ else{
 		<link rel='stylesheet' href='animasi/login/ayam.css'>
 		<link rel='stylesheet' href='aset/cinta.css'>
 		<link href='css/main.css' rel='stylesheet' type='text/css' media='all'/>
-		    <link rel='stylesheet' href='aset/bootstrap.css'>
-		<div class='errorpage'> <center><div class='danger'><i class='fa fa-exclamation-triangle'></i></div><br><h1>LOGIN GAGAL!</h1>
-        Username dan Password anda salah.<br><br><input name='submit' id='submitku' type=submit style='padding: 6px 12px;' value='ULANGI LAGI' onclick=location.href='formlogin'></a><br><p class='message'>Masih bingung, Kembali ke <a href='bantuan'>Halaman Bantuan</a></p></center></div>
-<div class='chick-wrapper-landing show'>
-  <div class='wing-back'></div>
-  <div class='body'>
-    <div class='eye-left'></div>
-    <div class='eye-right'></div>
-  </div>
-  <div class='wing-front'></div>
-</div>
-<div class='chick-wrapper-run run'><img class='egg-lay' src='animasi/login/lay_egg.png'/>
-  <div class='legs'>
-    <div class='leg-l'></div>
-    <div class='leg-r'></div>
-  </div>
-  <div class='wing-back'> </div>
-  <div class='sweat-1'></div>
-  <div class='sweat-2'></div>
-  <div class='sweat-3'></div>
-  <div class='body'>
-    <div class='eye-liner'>
-      <div class='eye'></div>
+    <link rel='stylesheet' href='aset/bootstrap.css'>
+		<div class='errorpage'> 
+      <center>
+        <div class='danger'><i class='fa fa-exclamation-triangle'></i></div>
+        <br>
+        <h1>LOGIN GAGAL!</h1>
+        Username dan Password anda salah.
+        <br><br>
+        <input name='submit' id='submitku' type=submit style='padding: 6px 12px;' value='ULANGI LAGI' onclick=location.href='formlogin'>
+        </a>
     </div>
-    <div class='eye-lid'></div>
-    <div class='cheek'></div>
-  </div>
-  <div class='sweat-last'></div>
-  <div class='wing-front'></div>
-</div>
+
     <script  src='animasi/login/index.js'></script>";
 }
 ?>
